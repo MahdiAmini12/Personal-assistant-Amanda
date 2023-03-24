79dfcea411eb3563ba4948b7d1d73b41cb1f2413
Mahdi-Amini <92228858+MahdiAmini12@users.noreply.github.com>
2023-03-24 07:36:34 +0330

    Update speech.py

--- Code/speech.py
+++ Code/speech.py
@@ -1,33 +1,66 @@
-import speech_recognition as sr
-import answer
-import keys 
+import pyttsx3 as pt
+import random
+import datetime
+import playsound
+import cv2
 
 
-            
-# Recognizer voice
-def setMic():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        r.pause_threshold = 0.5
-        audio = r.listen(source)
-        print("Recognizing...")
-        query = r.recognize_google(audio, language="en")
-        print(query)
-    return query
+
+gender = 0
+engine = pt.init()
+name = ''
+
+engine.setProperty('voice', engine.getProperty('voices')[1].id)
 
 
-# keyItem تعریف
-while True:
-    try:
-        query = setMic().lower()
-        for keyEl in keys.keyItems:
-            if keyEl in query:
-                if keys.keyItems[keyEl][1]:
-                    answer.say(f'tell me {keys.keyItems[keyEl][1]}')
-                    ans = setMic().lower()
-                    keys.keyItems[keyEl][0](ans)
-                else:
-                    keys.keyItems[keyEl][0](query)
-    except Exception as e:
-        print(e)
+
+def setName(query):
+    with open('namesave.txt', 'w') as sName:
+        sName.write(query)
+    pt.speak(f'Ok i changed your name to {query}')
+
+
+def whatIsMyName(query):
+    with open('namesave.txt', 'r') as name:
+        pt.speak('your name is ' + name.read())
+
+
+# Amanda voice
+def setRate(type):
+    speed = engine.getProperty('rate')
+    speed += 50 if type == 'increase' else 0
+    speed -= 50 if type == 'decrease' else 0
+    engine.setProperty('rate', speed)
+    pt.speak('Ok im ready')
+
+
+def changeGender(text):
+    global gender, engine
+    if ('man' in text and 'woman' not in text) or ('men' in text and 'women' not in text):
+        gender = 0
+        text = 'man'
+    elif 'woman' in text or 'women' in text:
+        gender = 1
+        text = 'woman'
+    engine.setProperty('voice', engine.getProperty('voices')[gender].id)
+    pt.speak(f'Ok im on {text} form')
+
+
+# Start greeting
+def wishMe():
+    hour = datetime.datetime.now().hour
+    if hour >= 0 and hour < 12:
+        pt.speak(f"Hello {name},Good Morning")
+    elif hour >= 12 and hour < 18:
+        pt.speak(f"Hello {name},Good Afternoon")
+    else:
+        playsound.playsound(r'ttsMP3.com_VoiceText_2023-1-4_20_41_15.mp3')
+
+# time
+def time(query):
+    strTime = datetime.datetime.now()
+    pt.speak(f"the time is {strTime.hour}:{strTime.minute}")
+
+
+def say(text):
+    pt.speak(text)
