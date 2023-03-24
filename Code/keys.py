0a99a2c9fbadc44237b402b9047f1b67eb2114d4
Mahdi-Amini <92228858+MahdiAmini12@users.noreply.github.com>
2023-03-24 19:49:41 +0330

    Update keys.py

--- Code/keys.py
+++ Code/keys.py
@@ -1,8 +1,10 @@
 import answer
+
 keyItems = {'set voice': (answer.changeGender, 'woman or man'),
             'change voice': (answer.changeGender, 'woman or man'),
             'change your voice': (answer.changeGender, 'woman or man'),
             'change speed': (answer.setRate, 'increase or decrease'),
             'set name': (answer.setName, 'your Name'),
             "what's my name": (answer.whatIsMyName, None),
-            "what is my name": (answer.whatIsMyName, None)}
+            "what is my name": (answer.whatIsMyName, None),
+            'time': (answer.time, None)}
