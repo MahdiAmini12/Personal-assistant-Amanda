b28ed9bbf5b58b5c5c495cd020f5ec42124c2bce
Mahdi-Amini <92228858+MahdiAmini12@users.noreply.github.com>
2023-03-24 06:57:21 +0330

    Create answer.py

---
+++ Code/answer.py
@@ -0,0 +1,58 @@
+import pyttsx3 as pt
+import random
+import datetime
+import playsound
+import cv2
+
+
+
+gender = 0
+engine = pt.init()
+
+engine.setProperty('voice', engine.getProperty('voices')[1].id)
+
+
+def whatIsMyName(query):
+    with open('namesave.txt', 'r') as name:
+        pt.speak('your name is ' + name.read())
+
+
+# Amanda voice
+def setRate(type):
+    speed = engine.getProperty('rate')
+    speed += 50 if type == 'increase' else 0
+    speed -= 50 if type == 'decrease' else 0
+    engine.setProperty('rate', speed)
+    pt.speak('Ok im ready')
+
+
+def changeGender(text):
+    global gender, engine
+    if ('man' in text and 'woman' not in text) or ('men' in text and 'women' not in text):
+        gender = 0
+        text = 'man'
+    elif 'woman' in text or 'women' in text:
+        gender = 1
+        text = 'woman'
+    engine.setProperty('voice', engine.getProperty('voices')[gender].id)
+    pt.speak(f'Ok im on {text} form')
+
+
+# Start greeting
+def wishMe():
+    hour = datetime.datetime.now().hour
+    if hour >= 0 and hour < 12:
+        pt.speak(f"Hello {name},Good Morning")
+    elif hour >= 12 and hour < 18:
+        pt.speak(f"Hello {name},Good Afternoon")
+    else:
+        playsound.playsound(r'ttsMP3.com_VoiceText_2023-1-4_20_41_15.mp3')
+
+# time
+def time(query):
+    strTime = datetime.datetime.now()
+    pt.speak(f"the time is {strTime.hour}:{strTime.minute}")
+
+
+def say(text):
+    pt.speak(text)
