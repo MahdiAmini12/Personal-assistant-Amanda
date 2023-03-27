0c6e483551d38c234b03a6196b7f2ab7653df396
Mahdi-Amini <92228858+MahdiAmini12@users.noreply.github.com>
2023-03-27 03:50:17 +0330

    Update keys.py

--- Code/keys.py
+++ Code/keys.py
@@ -1,7 +1,9 @@
-import answer
 import utilities
+import answer
+import Talk
 
-keyItems = {'set voice': (answer.changeGender, 'woman or man'),
+keyItems = {#answer
+            'set voice': (answer.changeGender, 'woman or man'),
             'change voice': (answer.changeGender, 'woman or man'),
             'change your voice': (answer.changeGender, 'woman or man'),
             'change speed': (answer.setRate, 'increase or decrease'),
@@ -9,6 +11,7 @@ keyItems = {'set voice': (answer.changeGender, 'woman or man'),
             "what's my name": (answer.whatIsMyName, None),
             "what is my name": (answer.whatIsMyName, None),
             'time': (answer.time, None),
+            #utilities
             'camera': (utilities.takePhoto, None),
             'take a photo': (utilities.takePhoto, None),
             'take your photo': (utilities.takePhoto, None),
@@ -16,4 +19,17 @@ keyItems = {'set voice': (answer.changeGender, 'woman or man'),
             'system information': (utilities.system_information, None),
             'CPU': (utilities.system_information, None),
             'RAM': (utilities.system_information, None),
-            'Battery percent': (utilities.system_information, None)}
+            'battery percent': (utilities.battery_percent, None),
+            #Talk
+            'how are you': (Talk.how, None),
+            'are you ok': (Talk.how, None),
+            'are you good': (Talk.how, None),
+            'how is it going': (Talk.how, None),
+            'thanks': (Talk.how, None),
+            'thank you': (Talk.how, None),
+            'who are you': (Talk.who, None),
+            'what can you do': (Talk.who, None),
+            'are you crazy': (Talk.crazy, None),
+            'you are crazy': (Talk.youcrazy, None),
+            'can I change your name': (Talk.changeAmandaName, None),
+            'change your name': (Talk.changeAmandaName, None),}
