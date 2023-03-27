f213d9c2d3f3574edfd6a6437e879a8eeed5ee07
Mahdi-Amini <92228858+MahdiAmini12@users.noreply.github.com>
2023-03-27 03:51:24 +0330

    Update utilities.py

--- Code/utilities.py
+++ Code/utilities.py
@@ -1,9 +1,12 @@
 import time
-import subprocess
-import webbrowser
 import psutil
 import pyttsx3 as pt
 from ecapture import ecapture as ec
+# import subprocess
+# import webbrowser
+# import requests
+# import json
+
 
 #نمایش اطلاعات سیستم
 def system_information(query):
@@ -23,6 +26,14 @@ def system_information(query):
     else:
         pt.spead("The system is not running on battery power.")
         print("The system is not running on battery power.")
+
+
+def battery_percent(query):
+    battery = psutil.sensors_battery()
+    percent = battery.percent
+
+    print(f"Battery percent: {battery.percent}%")
+    pt.speak(f"Battery percent: {battery.percent}%")
 
 
 
